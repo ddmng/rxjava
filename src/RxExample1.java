0d9c99d16ea55e199b23a63b092988cf19c93289
daniele.diminica@gmail.com <mettiamounpodirobaquasopra>
2015-10-23 22:46:20 +0200

    First test

---
+++ src/RxExample1.java
@@ -0,0 +1,48 @@
+import rx.Observable;
+import rx.functions.Action0;
+import rx.functions.Action1;
+
+class RxExample1 {
+    public static void sayHello(String[] names) {
+        Observable.from(names).subscribe(
+                new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        System.out.println("Hello " + s + "!");
+                    }
+
+                },
+                new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable i) {
+                        System.out.println("Errore");
+                    }
+
+                },
+                new Action0() {
+                    @Override
+                    public void call() {
+                        System.out.println("Completo");
+                    }
+
+                });
+    }
+
+
+    public static void sayHelloAlways(String... names) {
+        Observable.from(names).repeat(5).subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                System.out.println("Hello " + s + "!");
+            }
+
+        });
+    }
+
+    public static void main(String args[]) {
+
+        sayHello(new String[]{"uno", "due", "tre"});
+        sayHelloAlways("one", "two", "three");
+    }
+
+}
